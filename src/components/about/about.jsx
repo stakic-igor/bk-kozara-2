db63e7c3506013f5cfba7cdfc60badc254ad351e
stakic-igor <stakic_igor@yahoo.com>
2019-12-16 23:39:18 +0100

    Add header image, title and images to articles

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,22 +10,35 @@ class About extends Component {
     }
 
     componentDidMount() {
-        const url = "http://localhost/bk-kozara/wordpress/wordpress/wp-json/wp/v2/posts?categories=3&order=asc";
+        const url = "http://localhost/bk-kozara/wordpress/wordpress/wp-json/wp/v2/posts?categories=3&order=asc&_embed";
         fetch(url)
             .then(res => res.json())
             .then(data => this.setState({
                 aboutPosts: data
             }))
+        }
+        
+        componentDidUpdate() {
+            console.log(this.state.aboutPosts)
+
     }
 
     render() {
 
         const { aboutPosts } = this.state;
+
         let post = aboutPosts.map(post =>
-                <div key={post.id}>
-                    <h3 className="heading">{post.title.rendered}</h3>
-                    <p dangerouslySetInnerHTML={{__html: post.content.rendered}}></p>
-                </div>
+            <div key={post.id} className="about-card">
+                    <div className="about-card__content">
+                        <h3 className="heading">{post.title.rendered}</h3>
+                        <div dangerouslySetInnerHTML={{__html: post.content.rendered}} className="about-card__content__paragraph"></div>
+                    </div>
+                    <img 
+                        src={post._embedded['wp:featuredmedia'][0].source_url}
+                        className="about-card__img"
+                        alt={post._embedded['wp:featuredmedia'][0].slug}
+                        />
+            </div>
             )
 
         return (
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
     return (
         <section className="header">
-            
+            <p className="header-teaser">Biciklistički klub <br /> Kozara ProCycling</p>
         </section>
     )
 }
